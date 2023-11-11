7aff674ba809277de78ea5ab3b91dc27d88412bf
domEscobar <neridonk@googlemail.com>
2023-11-11 19:16:05 +0100

    import

---
+++ sdk/main.ts
@@ -0,0 +1,250 @@
+const key = "test";
+const serverUrl = "http://localhost:3440/execute";
+
+async function codeExec(code: string): Promise<any> {
+    const req: ExternalCodeRequest = { code, key };
+    const response = await fetch(serverUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req),
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.result;
+}
+
+// Opens the app with the given options
+export async function openApp(options?: { width?: number, height?: number, focus?: boolean, bringToFront?: boolean, prompt?: string }) {
+    const code = `await openApp(${JSON.stringify(options ?? {})});`;
+    await codeExec(code);
+}
+
+// Saves the text to the instance knowledge base
+export async function ingestText(text: string, metadata?: any): Promise<void> {
+    const code = `await ingestText(\`${text}\`, ${JSON.stringify(metadata ?? {})});`;
+    await codeExec(code);
+}
+
+// TODO
+async function ingestFile(path: string, metadata?: any) {
+    const code = `await ingestFile(\`${path}\`, ${JSON.stringify(metadata ?? {})});`;
+    await codeExec(code);
+}
+
+// Opens a new interaction
+export async function newInteraction(): Promise<string> {
+    const code = `return await newInteraction();`;
+    const conversationId = await codeExec(code);
+    return conversationId;
+}
+
+// Gets the current interaction results 
+export async function getInteractionResults(): Promise<any[]> {
+    const code = `return await getInteractionResults();`;
+    const results = await codeExec(code);
+    return results;
+}
+
+// Content for direct interaction e.g QA on a specific text
+export async function addInteractionContent(text: string): Promise<void> {
+    const code = `await addInteractionContent(\`${text}\`);`;
+    await codeExec(code);
+}
+
+// Gets the current screen data, buffer needs to be parsed with JSON.parse
+export async function getScreenData(): Promise<ScreenData> {
+    const code = "return await getScreenData();";
+    const data = await codeExec(code);
+    const buffer = Buffer.from(JSON.parse(data.fileBuffer));
+    return { screen: data.screen, fileBuffer: buffer } as ScreenData;
+}
+
+// Gets the current active window data, buffer needs to be parsed with JSON.parse
+export async function getActiveWindowData(): Promise<ScreenData> {
+    const code = "return await getActiveWindowData();";
+    const data = await codeExec(code);
+    const buffer = Buffer.from(JSON.parse(data.fileBuffer));
+    return { screen: data.screen, fileBuffer: buffer } as ScreenData;
+}
+
+// Sets the current interaction prompt
+export async function setInput(text: string): Promise<void> {
+    const code = `await setInput(\`${text}\`);`;
+    await codeExec(code);
+}
+
+// Submits and starts the interaction
+export async function submitPrompt(text?: string): Promise<void> {
+    const code = `await submitPrompt(\`${text}\`);`;
+    await codeExec(code);
+}
+
+export async function executeCommand(input: string, cmdId: string): Promise<void> {
+    const code = `return await execCommand(\`${input}\`, \`${cmdId}\`);`;
+    return await codeExec(code);
+}
+
+export async function addPrompt(cmd: string, taskName: string = "", sus: any[] = []): Promise<void> {
+    const code = `await addInteractionPrompt(getConvoId(), \`${cmd}\`, \`${taskName}\`, ${JSON.stringify(sus)});`;
+    await codeExec(code);
+}
+
+export async function updatePrompt(result: string): Promise<void> {
+    const code = `await updatePrompt(\`${result}\`);`;
+    await codeExec(code);
+}
+
+export async function addResult(result: string): Promise<void> {
+    const code = `await addResult(\`${result}\`);`;
+    await codeExec(code);
+}
+
+export async function pushResultLog(log: string): Promise<void> {
+    const code = `await pushResultLog(\`${log}\`);`;
+    await codeExec(code);
+}
+
+export async function pushHtml(html: string): Promise<void> {
+    const code = `await pushHtml(\`${html}\`);`;
+    await codeExec(code);
+}
+
+export async function pushResultStream(token: string): Promise<void> {
+    const code = `await pushResultStream(\`${token}\`);`;
+    await codeExec(code);
+}
+
+export async function pushInlineConfirm(text: string, buttons: { text: string, action: () => void }[]): Promise<void> {
+    const buttonsenc = buttons ? buttons.map(b => ({ ...b, action: b.action.toString() })) : null;
+    const code = `await pushInlineConfirm(\`${text}\`, ${buttons ? JSON.stringify(buttonsenc) : null});`;
+    await codeExec(code);
+}
+
+export async function startAudioRecording(): Promise<any[]> {
+    const code = `return await startAudioRecording();`;
+    const autidoChunks = await codeExec(code);
+    return autidoChunks;
+}
+
+export async function stopAudioRecording(): Promise<void> {
+    const code = `stopAudioRecording();`;
+    await codeExec(code);
+}
+
+export async function getMarkedText(): Promise<string> {
+    const code = `return await getMarkedText();`;
+    const markedText = await codeExec(code);
+    return markedText;
+}
+
+export async function getWindowText(): Promise<string> {
+    const code = `return await getWindowText();`;
+    const selectedText = await codeExec(code);
+    return selectedText;
+}
+
+export async function getActiveWindowTitle(): Promise<string> {
+    const code = `return await activeWindowTitle();`;
+    const title = await codeExec(code);
+    return title;
+}
+
+export async function getCursorPosition(): Promise<CursorPosition> {
+    const code = `return await cursorPosition();`;
+    const cursorPos = await codeExec(code);
+    return cursorPos;
+}
+
+export async function moveMouseTo(x: number, y: number): Promise<void> {
+    const code = `await moveMouseTo(${x}, ${y});`;
+    await codeExec(code);
+}
+
+export async function mouseClickLeft(): Promise<void> {
+    const code = `await mouseClick();`;
+    await codeExec(code);
+}
+
+export async function pressEnter(): Promise<void> {
+    const code = `await pressEnter();`;
+    await codeExec(code);
+}
+
+export async function clickText(text: string): Promise<void> {
+    const code = `await clickText(\`${text}\`);`;
+    await codeExec(code);
+}
+
+export async function writeText(text: string): Promise<void> {
+    const code = `await writeText(\`${text}\`);`;
+    await codeExec(code);
+}
+
+export async function pasteText(text: string): Promise<void> {
+    const code = `await pasteText(\`${text}\`);`;
+    await codeExec(code);
+}
+
+// define an area by dragging the mouse
+export async function getAreaData(): Promise<void> {
+    const code = `return await getAreaData();`;
+    return await codeExec(code);
+}
+
+export async function openNewWindow(htmlOrFilePath: string, options?: BrowserWindowOptions): Promise<void> {
+    const code = `await openBrowserWindow(\`${htmlOrFilePath}\`, ${JSON.stringify(options ?? {})});`;
+    await codeExec(code);
+}
+
+export async function displayCursorContent(html: string): Promise<void> {
+    const code = `await displayCursorContent(\`${html}\`);`;
+    await codeExec(code);
+}
+
+export async function hideCursorContent(): Promise<void> {
+    const code = `await hideCursorContent();`;
+    await codeExec(code);
+}
+
+export interface ExternalCodeRequest {
+    key: string;
+    code: string;
+}
+
+export interface ScreenData {
+    screen: { id: string, name: string, display_id: string };
+    fileBuffer: Buffer;
+}
+
+export interface CursorPosition {
+    x: number;
+    y: number;
+}
+
+export interface BrowserWindowOptions {
+    browserWindowOptions?: any//Electron.BrowserWindowConstructorOptions,
+    position?: { x: number, y: number },
+    size?: { width: number, height: number },
+    bringToFront?: boolean
+    focus?: boolean,
+    asFile?: string,
+    code: string
+}
+
+export interface LlmResultModel {
+    input: string;
+    result?: string;
+    date: Date;
+    resultDate?: Date;
+    taskName?: string;
+    sources?: any[];
+    html?: string[];
+    logs?: string[];
+    inlineElements?: any[];
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { openApp } from "../sdk/main";
+
+async function main() {
+    await openApp();
+}
+
+main();
+
